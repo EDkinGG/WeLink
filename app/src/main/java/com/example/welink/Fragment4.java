13e5c323d4016fcfc0b07ee19a56c5c5d95ab45d
EDkinGG <r23ghani@gmail.com>
2022-10-19 23:42:40 +0600

    v38

--- app/src/main/java/com/example/welink/Fragment4.java
+++ app/src/main/java/com/example/welink/Fragment4.java
@@ -1,15 +1,23 @@
 package com.example.welink;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,13 +28,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.security.Permission;
+import java.util.List;
 
 public class Fragment4 extends Fragment implements View.OnClickListener{
     Button button;
@@ -34,6 +51,8 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference,likeref;
     Boolean likechecker = false;
+    DatabaseReference db1, db2, db3;
+
 
     @Nullable
     @Override
@@ -54,6 +73,14 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
         recyclerView = getActivity().findViewById(R.id.rv_posts);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentuid = user.getUid();
+
+        db1 = database.getReference("All images").child(currentuid);
+        db2 = database.getReference("All videos").child(currentuid);
+        db3 = database.getReference("All posts");
 
         button.setOnClickListener(this);
     }
@@ -90,17 +117,19 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
                         final String postkey = getRef(position).getKey();
                         holder.SetPost(getActivity(), model.getName(), model.getUrl(), model.getPostUri(),model.getTime(),model.getUid(),model.getType(), model.getDesc());
 
+                        final String url = getItem(position).getPostUri();
                         final String name = getItem(position).getName();
-                        final String url = getItem(position).getUrl();
+                       // final String url = getItem(position).getUrl();
                         final String time = getItem(position).getTime();
                         final String userid = getItem(position).getUid();
+                        final String type = getItem(position).getType();
 
                         holder.likeschecker(postkey);
 
                         holder.menuoptions.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
-                                showDialog(name,url,time,userid);
+                                showDialog(name,url,time,userid,type);
                             }
                         });
 
@@ -151,7 +180,7 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
 
     }
 
-    void showDialog(String  name , String  url , String  time , String userid )
+    void showDialog(String  name , String  url , String  time , String userid, String type )
     {
         LayoutInflater inflater = LayoutInflater.from(getActivity());
         View view = inflater.inflate(R.layout.post_options,null);
@@ -165,6 +194,199 @@ public class Fragment4 extends Fragment implements View.OnClickListener{
                 .create();
 
         alertDialog.show();
+
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentUserid = user.getUid();
+
+        if(userid.equals(currentUserid)){
+            delete.setVisibility(View.VISIBLE);
+        }else{
+            delete.setVisibility(View.INVISIBLE);
+        }
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Query query = db1.orderByChild("time").equalTo(time);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot dataSnapshot1: snapshot.getChildren()){
+                            dataSnapshot1.getRef().removeValue();
+
+                            Toast.makeText(getActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+                Query query2 = db2.orderByChild("time").equalTo(time);
+                query2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot dataSnapshot1: snapshot.getChildren()){
+                            dataSnapshot1.getRef().removeValue();
+
+                            Toast.makeText(getActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+                Query query3 = db3.orderByChild("time").equalTo(time);
+                query3.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot dataSnapshot1: snapshot.getChildren()){
+                            dataSnapshot1.getRef().removeValue();
+
+                            Toast.makeText(getActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+                StorageReference reference = FirebaseStorage.getInstance().getReferenceFromUrl(url);
+                reference.delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(getActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                alertDialog.dismiss();
+            }
+        });
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               PermissionListener permissionListener = new PermissionListener(){
+                   @Override
+                   public void onPermissionGranted(){
+                       if(type.equals("iv")){
+                           DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                           request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI|
+                                   DownloadManager.Request.NETWORK_MOBILE);
+                           request.setTitle("Download");
+                           request.setDescription("Downloading image....");
+                           request.allowScanningByMediaScanner();
+                           request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                           request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,name+System.currentTimeMillis() + ".jpg");
+                           DownloadManager manager = (DownloadManager)getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+                           manager.enqueue(request);
+
+                           Toast.makeText(getActivity(), "Downloading", Toast.LENGTH_SHORT).show();
+
+                           alertDialog.dismiss();
+                       }else{
+                           DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                           request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI|
+                                   DownloadManager.Request.NETWORK_MOBILE);
+                           request.setTitle("Download");
+                           request.setDescription("Downloading video....");
+                           request.allowScanningByMediaScanner();
+                           request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                           request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,name+System.currentTimeMillis() + ".mp4");
+                           DownloadManager manager = (DownloadManager)getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+                           manager.enqueue(request);
+
+                           Toast.makeText(getActivity(), "Downloading", Toast.LENGTH_SHORT).show();
+
+                           alertDialog.dismiss();
+                       }
+                   }
+
+                   @Override
+                   public void onPermissionDenied(List<String> deniedPermissions){
+                       Toast.makeText(getActivity(),"No permissions", Toast.LENGTH_SHORT).show();
+                   }
+               };
+
+               TedPermission.create()
+                       .setPermissionListener(permissionListener)
+                       .setPermissions(Manifest.permission.INTERNET,Manifest.permission.READ_EXTERNAL_STORAGE)
+                       .check();
+
+                if(type.equals("iv")){
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI|
+                            DownloadManager.Request.NETWORK_MOBILE);
+                    request.setTitle("Download");
+                    request.setDescription("Downloading image....");
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,name+System.currentTimeMillis() + ".jpg");
+                    DownloadManager manager = (DownloadManager)getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+                    manager.enqueue(request);
+
+                    Toast.makeText(getActivity(), "Downloading", Toast.LENGTH_SHORT).show();
+
+                    alertDialog.dismiss();
+                }else{
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI|
+                            DownloadManager.Request.NETWORK_MOBILE);
+                    request.setTitle("Download");
+                    request.setDescription("Downloading video....");
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,name+System.currentTimeMillis() + ".mp4");
+                    DownloadManager manager = (DownloadManager)getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+                    manager.enqueue(request);
+
+                    Toast.makeText(getActivity(), "Downloading", Toast.LENGTH_SHORT).show();
+
+                    alertDialog.dismiss();
+                }
+            }
+        });
+
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String sharetext = name + "\n" +"\n"+ url;
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra(Intent.EXTRA_TEXT,sharetext);
+                intent.setType("text/plain");
+                startActivity(intent.createChooser(intent,"share via"));
+
+                alertDialog.dismiss();
+            }
+        });
+
+
+        copyurl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager cp = (ClipboardManager)getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("String",url);
+                cp.setPrimaryClip(clip);
+                clip.getDescription();
+                Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
+
+                alertDialog.dismiss();
+            }
+        });
     }
 
 }
